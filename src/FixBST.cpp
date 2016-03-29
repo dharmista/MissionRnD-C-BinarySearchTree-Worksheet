e9b870084853761d1790a375bc650f9e061c28ae
dharmista <dharmista@missionrnd.com>
2016-03-29 11:36:27 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -32,6 +32,36 @@ struct node{
 	struct node *right;
 };
 
-void fix_bst(struct node *root){
+void inorder_prev(node * root, node ** k, node ** n1, node ** n2){
+	if (root == NULL)
+		return;
+	inorder_prev(root->left, k, n1, n2);
+	if (k[0] != NULL){
+		if (root->data < k[0]->data){
+			if (*n1 == NULL)
+			{
+				*n1 = k[0];
+				*n2 = root;
+			}
+			else
+				*n2 = root;
+		}
+	}
+	k[0] = root;
+	inorder_prev(root->right, k, n1, n2);
+}
 
+void swap_nodes(struct node *a, struct node *b){
+	int temp = a->data;
+	a->data = b->data;
+	b->data = temp;
+}
+
+void fix_bst(node * root){
+	if (root == NULL)
+		return;
+	node * n1 = NULL, *n2 = NULL, **k = (node **)malloc(sizeof(node));
+	k[0] = NULL;
+	inorder_prev(root, k, &n1, &n2);
+	swap_nodes(n1, n2);
 }
